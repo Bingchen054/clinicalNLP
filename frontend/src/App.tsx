15840251c338f425feadc9c7632ff1db613c7d97
Bingchen054 <bingchen054@gmail.com>
2026-02-19 18:20:02 -0800

    fix router basename for github pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/clinicalNLP">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/input" element={<InputPage />} />
